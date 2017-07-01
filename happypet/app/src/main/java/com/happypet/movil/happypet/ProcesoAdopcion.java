fee8af6f56596236070513774d3a71d5a1619546
DESKTOP-4LM1QNA\User <ikarinacp@hotmail.com>
2017-07-01 10:32:18 -0500

    x

---
+++ happypet/app/src/main/java/com/happypet/movil/happypet/ProcesoAdopcion.java
@@ -0,0 +1,13 @@
+package com.happypet.movil.happypet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProcesoAdopcion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proceso_adopcion);
+    }
+}
